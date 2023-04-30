022f296db00274a8662710b63d45ba064de5b3c4
Jarosław Partyka <partyka.jaroslaw@gmail.com>
2023-04-30 16:24:25 +0200

    added greeting

--- django/lecture3/hello/views.py
+++ django/lecture3/hello/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+
 def index(request):
     return HttpResponse("Hello, world! TEST")
 
@@ -9,4 +11,10 @@ def jarek(request):
     return HttpResponse("Hello, Jarek!")
 
 def greet(request, name):
-    return HttpResponse(f"Hello, {name.capitalize()}")
+    return render(request, "hello/greet/html"), {
+        "name": name.capitalize()
+    }
+
+def index(request):
+    return render(request="hello/index.html")
+
